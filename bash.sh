d0298c12b2767c4ef86a3ce44055d396049e30bf
Peyman Obeidy <99184357+pobeidy1@users.noreply.github.com>
2023-09-04 21:07:15 +1000

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/amine0110/Liver-Segmentation-Using-Monai-and-PyTorch
+
+# Change directory to the cloned repository
+cd Liver-Segmentation-Using-Monai-and-PyTorch
+
+# Install Monai
+pip install monai
+
+# Install requirements from the requirements.txt file
+pip install -r requirements.txt
+Save the file and make it executable using the following command:
+
+
+#chmod +x setup_liver_segmentation.sh
+#./setup_liver_segmentation.sh
 
